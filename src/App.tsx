adcaeeae97d95417ea3dc3f3b5140f130aa2dbf3
PeanutEz <phamlac10@gmail.com>
2026-01-14 22:40:14 +0700

    Lac Update Route

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/demo-useState" element={<DemoUseState />} />
+      <Route path="/demo-usestate" element={<DemoUseState />} />
     </Routes>
   );
 }
